4f3ebe955c5526ef7a6eb9e783496438ddd317ba
Bruno Furtado <brunofurmon@poli.ufrj.br>
2017-04-30 20:41:11 -0300

    Unused Imports

--- Arc/ArcWindow.cs
+++ Arc/ArcWindow.cs
@@ -1,16 +1,12 @@
 ﻿using Arc.Services;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using static Arc.Services.ArcConstants;
+
 
 namespace Arc
 {
